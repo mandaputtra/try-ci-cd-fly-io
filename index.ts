80c0af448e91a79e4bcdfdaafab071a692647568
Manda Putra <mandaputra8@gmail.com>
2023-12-14 21:14:46 +0700

    feat: listen to 0.0.0.0

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import App from './server'
+import App from "./server";
 const PORT = 3000;
 
-new App().app.listen(PORT, () => {
+new App().app.listen(PORT, "0.0.0.0", () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
