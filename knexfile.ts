15c824844a8fa440eb82c32e3f8db33275362093
Manda Putra <mandaputra8@gmail.com>
2023-12-14 21:02:19 +0700

    feat: postgres config

--- knexfile.ts
+++ knexfile.ts
@@ -2,24 +2,26 @@ import type { Knex } from "knex";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env["DATABASE_URL"]);
+
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "postgresql",
+    client: "pg",
     connection: process.env["DATABASE_URL"],
     pool: {
-      min: 2,
-      max: 10,
+      min: 0,
+      max: 1,
     },
     migrations: {
       tableName: "knex_migrations",
     },
   },
   production: {
-    client: "postgresql",
+    client: "pg",
     connection: process.env["DATABASE_URL"],
     pool: {
-      min: 2,
-      max: 10,
+      min: 0,
+      max: 1,
     },
     migrations: {
       tableName: "knex_migrations",
